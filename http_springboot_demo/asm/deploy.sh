1574ee6bb479ed47efa1c0bc36696c976b93932e
lu.hl <lu.hl@alibaba-inc.com>
2020-06-27 15:15:50 +0800

    up

--- http_springboot_demo/asm/deploy.sh
+++ http_springboot_demo/asm/deploy.sh
@@ -8,7 +8,7 @@ cd "$SCRIPT_PATH" || exit
 echo "setup deployment"
 kubectl \
   --kubeconfig ~/shop/bj_config \
-  apply -f asm/data_plane/http_springboot_deployment.yaml
+  apply -f data_plane/http_springboot_deployment.yaml
 
 kubectl \
   --kubeconfig ~/shop/bj_config \
@@ -17,28 +17,28 @@ kubectl \
 echo "setup service"
 kubectl \
   --kubeconfig ~/shop/bj_config \
-  apply -f asm/data_plane/http_springboot_service.yaml
+  apply -f data_plane/http_springboot_service.yaml
 
 kubectl \
   --kubeconfig ~/shop/bj_config \
   get po
 
-echo "setup ingress gateway"
-CID=
-sed "s#{CLUSTER_ID}#$CID#g" asm/data_plane/http_springboot_ingressgateway.yaml.template >/tmp/http_springboot_ingressgateway.yaml
-kubectl \
-  --kubeconfig ~/shop/zjk_asm_config \
-  apply -f /tmp/http_springboot_ingressgateway.yaml
+#echo "setup ingress gateway"
+#CID=
+#sed "s#{CLUSTER_ID}#$CID#g" data_plane/http_springboot_ingressgateway.yaml.template >/tmp/http_springboot_ingressgateway.yaml
+#kubectl \
+#  --kubeconfig ~/shop/zjk_asm_config \
+#  apply -f /tmp/http_springboot_ingressgateway.yaml
 
 echo "setup gateway"
 kubectl \
   --kubeconfig ~/shop/zjk_asm_config \
-  apply -f asm/control_plane/http_springboot_gateway.yaml
+  apply -f control_plane/http_springboot_gateway.yaml
 
 echo "setup virtual service"
 kubectl \
   --kubeconfig ~/shop/zjk_asm_config \
-  apply -f asm/control_plane/http_springboot_virtualservice.yaml
+  apply -f control_plane/http_springboot_virtualservice.yaml
 
 kubectl \
   --kubeconfig ~/shop/zjk_asm_config \
@@ -51,7 +51,7 @@ kubectl \
 echo "setup destination rule"
 kubectl \
   --kubeconfig ~/shop/zjk_asm_config \
-  apply -f asm/control_plane/http_springboot_destinationrule.yaml
+  apply -f control_plane/http_springboot_destinationrule.yaml
 
 kubectl \
   --kubeconfig ~/shop/zjk_asm_config \
