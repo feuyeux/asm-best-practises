2ce8883923d5d6f6b082508ea5757997650a48ef
lu.hl <lu.hl@alibaba-inc.com>
2020-07-10 16:08:02 +0800

    up

--- http_springboot_demo/asm/kube_info.sh
+++ http_springboot_demo/asm/kube_info.sh
@@ -8,4 +8,21 @@ source asm.config
 alias k="kubectl --kubeconfig $USER_CONFIG"
 
 v3_pod=$(k get pod -l app=http-hello-deploy -n hello -o jsonpath='{.items[2].metadata.name}')
-k -n hello get pod $v3_pod --export=true -o yaml > "$v3_pod.yaml"
+k -n hello get pod "$v3_pod" -o yaml >"/tmp/$v3_pod.yaml"
+
+printf "/var/run/secrets/istio/ (istiod-ca-cert)\n"
+k exec "$v3_pod" -c istio-proxy -n hello -- ls -al /var/run/secrets/istio
+
+printf "\n/etc/istio/proxy (istio-envoy)\n"
+k exec "$v3_pod" -c istio-proxy -n hello -- ls -al /etc/istio/proxy
+
+printf "\nenvoy-rev0.json\n"
+k exec "$v3_pod" -c istio-proxy -n hello -- cat /etc/istio/proxy/envoy-rev0.json
+
+printf "\n/etc/istio/pod (istio-podinfo)\n"
+k exec "$v3_pod" -c istio-proxy -n hello -- ls -al /etc/istio/pod
+
+printf "\n/var/lib/istio/data (istio-data)\n"
+k exec "$v3_pod" -c istio-proxy -n hello -- ls -al /var/lib/istio/data
+
+#k exec -it "$v3_pod" -c istio-proxy -n hello -- /bin/bash
