1d10b62e906bfdc6af9ce6e5fcd2366902e33ffa
lu.hl <lu.hl@alibaba-inc.com>
2020-08-13 21:32:14 +0800

    best practises

--- http_workload_traffic_demo/asm/asm.deploy.sh
+++ http_workload_traffic_demo/asm/asm.deploy.sh
@@ -9,28 +9,28 @@ source ../hybrid.config
 alias k="kubectl --kubeconfig $USER_CONFIG"
 alias m="kubectl --kubeconfig $MESH_CONFIG"
 
-k delete svc hello2-svc -n external-hello
-m delete namespace external-hello
+k delete svc hello2-svc -n external-hello >/dev/null 2>&1
+m delete namespace external-hello >/dev/null 2>&1
 m create ns external-hello
 m label ns external-hello istio-injection=enabled
 
-MESH_ID=c9e8589cc0afa430c80bc1541b6d61f0f
+MESH_ID=$(head -n 1 "$MESHID_CONFIG")
 
 aliyun servicemesh AddVmAppToMesh \
---ServiceMeshId $MESH_ID \
---Namespace external-hello \
---ServiceName hello2-svc \
---Ips "$VM_PRI_1","$VM_PRI_2","$VM_PRI_3" \
---Ports http:8001 \
---Labels app=hello-workload
+  --ServiceMeshId $MESH_ID \
+  --Namespace external-hello \
+  --ServiceName hello2-svc \
+  --Ips "$VM_PRI_1","$VM_PRI_2","$VM_PRI_3" \
+  --Ports http:8001 \
+  --Labels app=hello-workload
 echo "done"
 
 k get svc hello2-svc -n external-hello -o yaml
 echo
-s get serviceentry mesh-expansion-hello2-svc -n external-hello -o yaml
+m get serviceentry mesh-expansion-hello2-svc -n external-hello -o yaml
 echo
-s get workloadentry mesh-expansion-hello2-svc-1 -n external-hello -o yaml
+m get workloadentry mesh-expansion-hello2-svc-1 -n external-hello -o yaml
 echo
-s get workloadentry mesh-expansion-hello2-svc-2 -n external-hello -o yaml
+m get workloadentry mesh-expansion-hello2-svc-2 -n external-hello -o yaml
 echo
-s get workloadentry mesh-expansion-hello2-svc-3 -n external-hello -o yaml
+m get workloadentry mesh-expansion-hello2-svc-3 -n external-hello -o yaml
--- http_workload_traffic_demo/test_traffic_shift.sh
+++ http_workload_traffic_demo/test_traffic_shift.sh
@@ -9,18 +9,18 @@ alias k="kubectl --kubeconfig $USER_CONFIG"
 IP=$(k -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 echo "Ingress gateway ip:$IP"
 echo "Warm-up..."
-for i in {1..10}; do
+for i in {1..20}; do
   curl -s "$IP":8002/hello/eric
   echo
 done
 echo "Test route n a loop"
 echo >test_traffic_shift_result
-for i in {1..20}; do
+for i in {1..100}; do
   resp=$(curl -s "$IP":8002/hello/eric)
   echo "$resp" >>test_traffic_shift_result
 done
 
 echo "expected 30%(Hello eric)-60%(Bonjour eric)-10%(Hola eric):"
-sort test_traffic_shift_result | uniq -c | sort -nrk1
+sort test_traffic_shift_result | grep -v "^[[:space:]]*$"| uniq -c | sort -nrk1
 
 rm -rf test_traffic_shift_result
