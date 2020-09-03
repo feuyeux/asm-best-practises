ced75f04c812167ec45cbe94dcd4b2635556364a
lu.hl <lu.hl@alibaba-inc.com>
2020-09-03 17:13:12 +0800

    up

--- http_hybrid_demo/vm/dns.fake.sh
+++ http_hybrid_demo/vm/dns.fake.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 SCRIPT_PATH="$(
-  cd "$(dirname "$0")" >/dev/null 2>&1
+  cd "$(dirname "$0")" >/dev/null 2>&1 || exit
   pwd -P
 )/"
 cd "$SCRIPT_PATH" || exit
-source ../hybrid.config
+source ../reciprocal.config
 alias k="kubectl --kubeconfig $USER_CONFIG"
-hello3_svc_ip=$(k get svc hello3-svc -n hybrid-hello -o jsonpath='{.spec.clusterIP}')
-echo "$hello3_svc_ip hello3-svc.hybrid-hello.svc.cluster.local" > dns_record
+hello3_svc_ip=$(k get svc hello3-svc -n http-reciprocal-hello -o jsonpath='{.spec.clusterIP}')
+echo "$hello3_svc_ip hello3-svc.http-reciprocal-hello.svc.cluster.local" >dns_record
 
-VMS=("$VM_PUB_2" "$VM_PUB_3")
+VMS=("$VM_PUB_1" "$VM_PUB_2" "$VM_PUB_3")
 for vm in "${VMS[@]}"; do
-  ssh root@"$vm" "sed -i '/hello3-svc.hybrid-hello.svc.cluster.local/d' /etc/hosts"
-  ssh root@"$vm" "cat >> /etc/hosts" < dns_record
+  if [ "$vm" ]; then
+    if [ ! -s "$vm" ]; then
+      ssh root@"$vm" "sed -i '/hello3-svc.http-reciprocal-hello.svc.cluster.local/d' /etc/hosts"
+      ssh root@"$vm" "cat >> /etc/hosts" <dns_record
+      echo "==================== $vm ===================="
+      ssh root@"$vm" "cat /etc/hosts"
+    fi
+  fi
 done
 rm -rf dns_record
-
-for vm in "${VMS[@]}"; do
-  echo "::::$vm::::"
-  ssh root@"$vm" "cat /etc/hosts"
-done
--- http_reciprocal_demo/asm/test_mesh.sh
+++ http_reciprocal_demo/asm/test_mesh.sh
@@ -11,17 +11,21 @@ hello1_pod=$(k get pod -l app=hello1-deploy -n http-reciprocal-hello -o jsonpath
 echo "Test access vm ip directly"
 VMS=("$VM_PRI_1" "$VM_PRI_2" "$VM_PRI_3")
 for vm in "${VMS[@]}"; do
-  k exec "$hello1_pod" -c hello-v1-deploy -n http-reciprocal-hello -- curl -s "$vm":8001/hello/eric
-  echo
+  if [ "$vm" ]; then
+    if [ ! -s "$vm" ]; then
+      k exec "$hello1_pod" -c hello-v1-deploy -n http-reciprocal-hello -- curl -s "$vm":8001/hello/eric
+      echo
+    fi
+  fi
 done
+
 echo
 echo "Test access hello2-svc"
 k exec "$hello1_pod" -c hello-v1-deploy -n http-reciprocal-hello \
--- curl -s hello2-svc.http-reciprocal-hello.svc.cluster.local:8001/hello/eric
+  -- curl -s hello2-svc.http-reciprocal-hello.svc.cluster.local:8001/hello/eric
 echo
 echo
 echo "Test access hello1-svc"
 k exec "$hello1_pod" -c hello-v1-deploy -n http-reciprocal-hello \
--- curl -s hello1-svc.http-reciprocal-hello.svc.cluster.local:8004/hello/eric
+  -- curl -s hello1-svc.http-reciprocal-hello.svc.cluster.local:8004/hello/eric
 echo
-
