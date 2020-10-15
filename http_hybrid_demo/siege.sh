89452edc399b2821c4e5622d9fae750c3f8b193e
lu.hl <lu.hl@alibaba-inc.com>
2020-10-15 20:13:34 +0800

    up

---
+++ http_hybrid_demo/siege.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+SCRIPT_PATH="$(
+  cd "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)/"
+cd "$SCRIPT_PATH" || exit
+source hybrid.config
+alias k="kubectl --kubeconfig $USER_CONFIG"
+IP=$(k -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+echo "Ingress gateway ip:$IP"
+curl -s "$IP":8003/hello/asm
+siege -c 5 "$IP":8003/hello/asm
