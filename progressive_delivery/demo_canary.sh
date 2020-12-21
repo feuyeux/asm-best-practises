50cf2653008951d1afea168fa4ba19251ba59500
feuyeux <feuyeux@gmail.com>
2020-12-21 21:45:06 +0800

    debug canary

--- progressive_delivery/demo_canary.sh
+++ progressive_delivery/demo_canary.sh
@@ -5,8 +5,6 @@ SCRIPT_PATH="$(
 )/"
 cd "$SCRIPT_PATH" || exit
 
-echo $SCRIPT_PATH
-
 source config
 alias k="kubectl --kubeconfig $USER_CONFIG"
 alias m="kubectl --kubeconfig $MESH_CONFIG"
@@ -14,6 +12,7 @@ alias h="helm --kubeconfig $USER_CONFIG"
 
 install_flagger_on_asm() {
   echo "Deploy flagger"
+  # flagger
   cd $FLAAGER_SRC
   k -n istio-system create secret generic istio-kubeconfig --from-file $MESH_CONFIG
   k -n istio-system label secret istio-kubeconfig istio/multiCluster=true
@@ -43,12 +42,11 @@ setup_test() {
   m label namespace test istio-injection=enabled
 }
 
-# flagger
 # install_flagger_on_asm
 
 # setup
 setup_test
-
+# podinfo
 echo "Create a deployment and a horizontal pod autoscaler"
 k apply -k $FLAAGER_SRC/kustomize/podinfo
 #loader
@@ -58,40 +56,31 @@ k apply -k $FLAAGER_SRC/kustomize/tester
 # canary
 echo "Deploy canary resources"
 k apply -f resources_canary/podinfo-canary.yaml
-#
-# https://docs.flagger.app/v/master/tutorials/prometheus-operator
-# k apply -f $SCRIPT_PATH/service_monitor.yaml
-# k apply -f $SCRIPT_PATH/metrics_requests_error_rate.yaml
-# k apply -f $SCRIPT_PATH/metrics_latency.yaml
-# k apply -f $SCRIPT_PATH/podinfo-canary.yaml
-echo "Waiting 15s"
-sleep 15s
+
+echo "Waiting 30s"
+sleep 30s
 echo "Verify"
 k -n test get svc,pod,hpa
-sleep 15s
 
-echo "Start load"
-loadtester=$(k -n test get pod -l "app=flagger-loadtester" -o jsonpath='{.items..metadata.name}')
-k -n test exec -it ${loadtester} -c loadtester -- hey -z 20m -c 10 -q 2 http://podinfo:9898 &
-
-sleep 15s
+# echo "Start load"
+# loadtester=$(k -n test get pod -l "app=flagger-loadtester" -o jsonpath='{.items..metadata.name}')
+# k -n test exec -it ${loadtester} -c loadtester -- hey -z 20m -c 10 -q 2 http://podinfo:9898 &
 
 echo "Change deployment/podinfo to 3.1.1"
 k -n test set image deployment/podinfo podinfod=stefanprodan/podinfo:3.1.1
-
-sleep 30s
 m -n test get virtualservice,destinationrule
 
 # verify canary
-# k -n test describe canary/podinfo
+for ((i = 1; i <= 10; i++)); do
+  k -n test describe canary/podinfo
+  sleep 5s
+done
 
-# watch kubectl --kubeconfig $USER_CONFIG get canaries --all-namespaces
+watch kubectl --kubeconfig $USER_CONFIG get canaries --all-namespaces
 
 # https://flagger.app/intro/faq.html#metrics
-prometheus_lb=$(k get svc -n istio-system -l app=prometheus -o jsonpath="{.items..status.loadBalancer.ingress[0].ip}")
-echo "http://$prometheus_lb:9090"
-
-
+# prometheus_lb=$(k get svc -n istio-system -l app=prometheus -o jsonpath="{.items..status.loadBalancer.ingress[0].ip}")
+# echo "http://$prometheus_lb:9090"
 
 ###########
 # k -n test set image deployment/podinfo podinfod=stefanprodan/podinfo:3.1.2
