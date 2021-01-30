5cb8d1ecec05ad2204671f40ac8effc8bd8770c6
feuyeux <feuyeux@gmail.com>
2021-01-30 17:20:02 +0800

    up

--- blue_green_demo/header/deploy.sh
+++ blue_green_demo/header/deploy.sh
@@ -10,15 +10,13 @@ source config
 alias k="kubectl --kubeconfig $USER_CONFIG"
 alias m="kubectl --kubeconfig $MESH_CONFIG"
 
-
-# echo "clean..."
-# k delete namespace http-hello >/dev/null 2>&1
-# m delete namespace http-hello >/dev/null 2>&1
-# echo "create"
-# k create ns http-hello
-# m create ns http-hello
-# m label ns http-hello istio-injection=enabled
-
+echo "clean..."
+k delete namespace http-hello >/dev/null 2>&1
+m delete namespace http-hello >/dev/null 2>&1
+echo "create"
+k create ns http-hello
+m create ns http-hello
+m label ns http-hello istio-injection=enabled
 
 echo "deploy"
 #部署数据平面
@@ -30,20 +28,20 @@ k -n http-hello wait --for=condition=ready pod -l app=hello2-deploy-v2
 k -n http-hello wait --for=condition=ready pod -l app=hello1-deploy-v1
 
 #查看服务状态
-k get services -n http-hello
+k get svc -n http-hello
 #查看POD状态
 k get pods -n http-hello
 
 hello2_v1_pod=$(k get pod -l app=hello2-deploy-v1 -n http-hello -o jsonpath={.items..metadata.name})
 
-# echo "Check from $hello2_v1_pod:"
-# k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s localhost:8001/hello/eric
-# echo
-# k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello3-svc:8001/hello/eric
-# echo
-# k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello2-svc:8001/hello/eric
-# echo
-# k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello1-svc:8001/hello/eric
-# echo
+echo "Check from $hello2_v1_pod:"
+k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s localhost:8001/hello/eric
+echo
+k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello3-svc:8001/hello/eric
+echo
+k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello2-svc:8001/hello/eric
+echo
+k exec "$hello2_v1_pod" -c hello-v1-deploy -n http-hello -- curl -s http://hello1-svc:8001/hello/eric
+echo
 
 m apply -f mesh
