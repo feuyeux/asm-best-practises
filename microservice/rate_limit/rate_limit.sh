18a4495f07e7203743e1d0c13ecf979d4ffd51a6
feuyeux <feuyeux@gmail.com>
2021-05-25 18:41:37 +0800

    up

--- microservice/rate_limit/rate_limit.sh
+++ microservice/rate_limit/rate_limit.sh
@@ -11,4 +11,17 @@ alias m="kubectl --kubeconfig $HOME/shop_config/kubeconfig/asm_production"
 
 k apply -f data_plane/rate_limit_configmap.yaml
 k apply -f ${ISTIO_HOME}/samples/ratelimit/rate-limit-service.yaml
-m apply -f control_plane/rate_limit_envoyfilter.yaml
+m apply -f control_planerate_limit_envoyfilter.yaml
+
+GATEWAY_URL=$(k -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+curl "http://$GATEWAY_URL/productpage"
+
+# 请求频率大于 10 req/min 时，触发本地限流
+WAIT=0.05
+
+GW=$(k -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+
+for ((i = 1; i <= 20; i++)); do
+  curl "http://$GW/productpage"
+  sleep $WAIT
+done
